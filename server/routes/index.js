b11212515a8536c19d86af9cebe4c7ebd945995b
Malik <willis.malik.r@gmail.com>
2024-01-31 05:27:43 -0600

    w/protect

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const { protect } = require('../middleware/checkTok')
 
-
+// with protect
 // different model routers
 router.use('/tasks', protect, require('./tasks'));
 router.use('/users', require('./user'));
